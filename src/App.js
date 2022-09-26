9c868fe5eaa154036338f3ceb2f99b4bccab95c9
Hardik Vora <hardik7304@gmail.com>
2022-09-26 15:00:00 -0400

    Template created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      counter: 0
+    }
+    this.myFunction = this.myFunction.bind(this);
+  }
+  
+  myFunction(e) {
+    alert("The value of counter is " + this.state.counter)
+    this.setState({ counter: this.state.counter + 1 })
+  }
 
-export default App;
+  render() {
+    return (
+      <div className="App">
+        <form>
+          <div class="mb-3">
+            <label for="textInput" class="form-label">Enter a sentence(s) and/or paragraph(s)</label>
+            <input type="text" class="form-control" id="textInput" />
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
